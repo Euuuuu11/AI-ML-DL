d153e3ba7c16d34c9fc671324d6d46f899f1ba6e
Jaegeun <zkfmakekd2@naver.com>
2022-07-13 19:00:47 +0900

    ZZZZZZZZZZZZ

--- keras/keras38_split.py
+++ keras/keras38_split.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-a = np.array(range(1, 11))
-size = 5
+a = np.array(range(1, 13))
+size = 6
 
 def split_x(dataset, size):
     aaa = []
     for i in range(len(dataset) - size + 1):
         subset = dataset[i : (i + size)]
         aaa.append(subset)
-        return np.array(aaa)
+    return np.array(aaa)
     
 bbb = split_x(a, size)
 print(bbb)
