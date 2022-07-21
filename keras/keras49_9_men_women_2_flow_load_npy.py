afcd508542a7dd580258183b8987e0226e0e6c8c
Jaegeun <zkfmakekd2@naver.com>
2022-07-21 20:37:11 +0900

    commit

---
+++ keras/keras49_9_men_women_2_flow_load_npy.py
@@ -0,0 +1,59 @@
+import numpy as np  
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn import datasets
+#1. 데이터
+x_train = np.load('d:/study_data/_save/_npy/keras49_9_train_x.npy')
+y_train = np.load('d:/study_data/_save/_npy/keras49_9_train_y.npy')
+x_test = np.load('d:/study_data/_save/_npy/keras49_9_test_x.npy')
+y_test = np.load('d:/study_data/_save/_npy/keras49_9_test_y.npy')
+test_set = np.load('d:/study_data/_save/_npy/keras49_9_test_set.npy')
+
+print(test_set.shape)
+
+#2. 모델 구성
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten
+
+model = Sequential()
+model.add(Conv2D(128, (2,2), input_shape=(150,150,3), activation='relu'))
+model.add(Conv2D(64, (2,2), activation='relu'))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+hist = model.fit(x_train, y_train, epochs=30,validation_split=0.2)
+# model.fit(xy_train[0][0], xy_train[0][1]) # 배치를 최대로 잡으면 이거도 가능
+# hist = model.fit_generator(xy_train, epochs=30, steps_per_epoch=32,  # 한 epoch 종료 시 마다 검증할 때 사용되는 검증 스텝 수를 지정   
+#                                           # 전체데이터/batch = 160/5 = 32
+#                     validation_data=xy_test,
+#                     validation_steps=4)
+# accuracy = hist.history['accuracy']
+# val_accuracy = hist.history['val_accuracy']
+# loss = hist.history['loss']
+# val_loss = hist.history['val_loss']
+
+# print('loss :', loss[-1])
+# print('val_loss :', val_loss[-1])
+# print('accuracy:', accuracy[-1])
+# print('val_accuracy :', val_accuracy[-1])
+
+#4. 평가, 예측
+# from tensorflow.keras.utils import to_categorical
+loss = model.evaluate(x_test,y_test)
+y_predict = model.predict(test_set)
+print(y_predict)
+y_predict = np.argmax(y_predict, axis=1)
+# y_predict = to_categorical(y_predict)
+
+print('loss : ', loss)
+print('나의 성별은 : ', y_predict)
+
+# 나의 성별은 :  [0. 1.]
+
+# [[9.9999988e-01 1.2235688e-07]]
+# loss :  [7.958006858825684, 0.6304128766059875]
+# 나의 성별은 :  [0]
