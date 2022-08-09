d353767695b163070bf906d2cba4e89315d63029
Jaegeun <zkfmakekd2@naver.com>
2022-08-09 10:48:30 +0900

    commit

---
+++ ml/ml24_상관계수.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+
+#1. 데이터
+datasets = load_iris()
+# print(datasets.feature_names)
+# ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+x = datasets['data']
+y = datasets['target']
+
+df = pd.DataFrame(x, columns=[['sepal length', 'sepal width', 'petal length', 'petal width']])
+# print(df)
+df['Target(Y)'] = y
+print(df)
+
+print("============================ 상관계수 히트 맵 ===========================")
+print(df.corr())
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(font_scale=1.2)
+sns.heatmap(data=df.corr(), square=True, annot=True, cbar=True)
+
+plt.show()
