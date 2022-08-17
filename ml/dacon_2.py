06c742ec40ae245661bde1d3042ce260d77ed988
Jaegeun <zkfmakekd2@naver.com>
2022-08-17 17:54:01 +0900

    commit

--- ml/dacon_2.py
+++ ml/dacon_2.py
@@ -98,9 +98,10 @@ print(test)
 
 
 # 모델 선언
+from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier, XGBRegressor
 from sklearn.linear_model import LogisticRegression
-model = XGBClassifier()
+model = RandomForestClassifier()
 
 # 분석할 의미가 없는 칼럼을 제거합니다.
 # 상관계수 그래프를 통해 연관성이 적은것과 - 인것을 빼준다.
