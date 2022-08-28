58bac8fab37c5167850c3859457f51d2aa90693d
Jaegeun <zkfmakekd2@naver.com>
2022-08-28 18:43:31 +0900

    commit

--- keras/keras13_EarlyStopping1_california.py
+++ keras/keras13_EarlyStopping1_california.py
@@ -21,12 +21,12 @@ print(datasets.DESCR)
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(100, input_dim=8,activation='selu'))
-model.add(Dense(80))
-model.add(Dense(80,activation='selu'))
-model.add(Dense(60))
-model.add(Dense(60,activation='selu'))
-model.add(Dense(10))
+model.add(Dense(128, input_dim=8,activation='selu'))
+model.add(Dense(64))
+model.add(Dense(64,activation='selu'))
+model.add(Dense(32))
+model.add(Dense(32,activation='selu'))
+model.add(Dense(16))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
