7b24da041bc349e2142ddc43ceadec0f7cfc2981
Jaegeun <zkfmakekd2@naver.com>
2022-08-28 18:45:22 +0900

    commit

--- keras/keras14_3_activation_california.py
+++ keras/keras14_3_activation_california.py
@@ -21,12 +21,12 @@ print(datasets.DESCR)
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(100, input_dim=8,activation='relu'))
-model.add(Dense(80))
-model.add(Dense(80,activation='relu'))
-model.add(Dense(60,activation='relu'))
-model.add(Dense(60,activation='relu'))
-model.add(Dense(10))
+model.add(Dense(128, input_dim=8,activation='relu'))
+model.add(Dense(64))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(16))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
