8ba66cea29735ce2e0f9424c64585d3b1f6ad2c8
Jaegeun <zkfmakekd2@naver.com>
2022-09-06 11:08:33 +0900

    commit

--- keras2/keras54_optimizer02_lr_mnist.py
+++ keras2/keras54_optimizer02_lr_mnist.py
@@ -1,80 +1,72 @@
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D   
-from tensorflow.keras.datasets import mnist,fashion_mnist, cifar10, cifar100
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPool2D
+from tensorflow.keras.datasets import mnist
 import numpy as np
-from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.callbacks import EarlyStopping
+import tensorflow as tf
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import time
 
-
-
-#1. 데이터
+# 1. 데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# print(x_train.shape, y_train.shape) # (60000, 28, 28) (60000, )
-# print(x_test.shape, y_test.shape)   # (10000, 28, 28) (10000, )
+print(x_train.shape, y_train.shape) # (60000, 28, 28) (60000,)
+print(x_test.shape, y_test.shape) # (10000, 28, 28) (10000,)
 
 x_train = x_train.reshape(60000, 28, 28, 1)
 x_test = x_test.reshape(10000, 28, 28, 1)
-print(x_train.shape)             # (60000, 28, 28, 1)
-# print(np.unique(y_train, return_counts=True))
+print(x_train.shape)
 
-from tensorflow.keras.utils import to_categorical
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-# print(np.unique(y_train, return_counts=True))
-# print(np.unique(y_test, return_counts=True))
-# print(y_train, y_test)
-# print(y_test.shape, y_train.shape)
+print(np.unique(y_train, return_counts=True))
+# (array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949],
+#   dtype=int64))
+y_train = pd.get_dummies(y_train)
+y_test = pd.get_dummies(y_test)
+print(y_train.shape) # (60000, 10) // output 10개 = 라벨값
 
 
-#2. 모델구성
-model  = Sequential()
-# model.add(Dense(units=10, input_shape=(3, ))) #(batch_size, input_dim) 
-# (input_dim + bias) * units = summary Param 갯수 (Dense모델)
+# 2. 모델구성
+model = Sequential()
+model.add(Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='same', input_shape=(28, 28, 1)))
+model.add(MaxPool2D()) 
+model.add(Conv2D(10, (2,2),padding='valid', activation='relu'))
+model.add(Conv2D(5, (2,2),padding='same', activation='relu'))
+model.add(Conv2D(4, (2,2),padding='valid', activation='relu'))
+model.add(Flatten())
+model.add(Dense(32, activation='relu')) 
+model.add(Dense(32, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+model.summary()
 
-model.add(Conv2D(filters=64, kernel_size=(3, 3)
-                 ,padding= 'same' ,
-                 input_shape=(28, 28, 1)))
-model.add(MaxPooling2D())
-model.add(Conv2D(32, (2,2), 
-                 padding="valid",  # 디폴트 값
-                 activation="relu"))
-model.add(Conv2D(8,(2,2),padding='valid', activation='relu'))
-model.add(Flatten())  # (N, )
-model.add(Dense(32, activation="relu"))
-model.add(Dense(32, activation="relu"))
-model.add(Dense(10, activation="softmax"))
+# 3. 컴파일, 훈련
+from tensorflow.python.keras.optimizer_v2 import adam, adadelta, adagrad, adamax
+from tensorflow.python.keras.optimizer_v2 import rmsprop, nadam
 
-#3. 컴파일, 훈련
-# from tensorflow.python.keras.optimizer_v1 import Adam, Adadelta, Adagrad, Adamax
-# from tensorflow.python.keras.optimizer_v1 import RMSprop, SGD, Nadam
-from tensorflow.python.keras.optimizer_v2 import  adam, adadelta, adagrad, adamax
-from tensorflow.python.keras.optimizer_v2 import  rmsprop, nadam
-# from keras.optimizers import Adam, Adadelta, Adagrad, Adamax
-# from keras.optimizers import RMSprop, SGD, Nadam
+learning_rate = 0.001 # defualt = 0.001
+optimizer = adagrad.Adagrad(learning_rate=learning_rate)
 
-learning_rate = 0.001
-optimizer = adam.Adam(learning_rate=learning_rate)
-# optimizer = adadelta.Adadelta(learning_rate=learning_rate)
-# optimizer = adagrad.Adagrad(learning_rate=learning_rate)
-# optimizer = adamax.Adamax(learning_rate=learning_rate)
-# optimizer = rmsprop.RMSProp(learning_rate=learning_rate)
-# optimizer = nadam.Nadam(learning_rate=learning_rate)
-model.compile(loss='mse', optimizer=optimizer)
-
-model.fit(x_train, y_train, epochs=50, batch_size=128)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+Es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50, restore_best_weights=True)
+start = time.time()
+log = model.fit(x_train, y_train, epochs=1000, batch_size=100, callbacks=[Es], validation_split=0.2)
+end = time.time()
 
 #4. 평가, 예측
 loss = model.evaluate(x_test, y_test)
-y_predict = model.predict([11])
+y_predict = model.predict(x_test)
+y_predict = tf.argmax(y_predict, axis=1)
+y_test = tf.argmax(y_test, axis=1)
+acc_sc = accuracy_score(y_test, y_predict)
+print('loss : ', loss)
+print('acc스코어 : ', acc_sc, 'optimizer: {0}  lr: {1}'.format(optimizer.__class__.__name__, learning_rate))
+print('시간: ', round(end-start, 2))
 
-print('loss : ', round(loss, 4), 'lr : ', learning_rate, '결과물 : ', y_predict)
-# loss :  2.931 lr :  0.0001 결과물 :  [[9.608019]]
-# loss :  36.7842 lr :  0.0001 결과물 :  [[0.9814846]]
-# loss :  2.5876 lr :  0.0001 결과물 :  [[11.1229315]]
-# loss :  2.5479 lr :  0.0001 결과물 :  [[10.746758]]
-# loss :  2.5325 lr :  0.0001 결과물 :  [[11.765221]]
-# loss :  2.3677 lr :  0.0001 결과물 :  [[10.604613]]
+# loss :  [0.0747639536857605, 0.9764999747276306]
+# acc스코어 :  0.9765
+# 시간:  152.1847949028015
 
-# optimizers = [adam.Adam, adadelta.Adadelta, adagrad.Adagrad, adamax.Adamax, rmsprop.RMSProp, nadam.Nadam]
+# loss :  [0.06618057936429977, 0.9807000160217285]
+# acc스코어 :  0.9807 optimizer: Adam  lr: 0.001
+# 시간:  162.79056692123413
 
-# for i in optimizers
