847bbcd44d45e22bf25d212c53130ff89af8c9b6
Jaegeun <zkfmakekd2@naver.com>
2022-09-15 10:33:20 +0900

    commit

---
+++ torch/torch07_train_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#1. 데이터
+# x = np.array([1,2,3,4,5,6,7,8,9,10])
+# y = np.array([1,2,3,4,5,6,7,8,9,10])
+x_train = np.array([1,2,3,4,5,6,7])
+x_test = np.array([8,9,10])
+y_train = np.array([1,2,3,4,5,6,7])
+y_test = np.array([8,9,10])
+
+### [11,12,13]을 예측 ####
+x_predict = np.array[(11,12,13)]
+
