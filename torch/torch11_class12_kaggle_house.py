65accb236da4da4cdc598f52c378517e423c2d23
Jaegeun <zkfmakekd2@naver.com>
2022-09-16 11:35:24 +0900

    commit

---
+++ torch/torch11_class12_kaggle_house.py
@@ -0,0 +1,321 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from typing import Counter
+
+#1. 데이터
+path = './_data/kaggle_house/'
+train_set = pd.read_csv(path + 'train.csv')
+test_set = pd.read_csv(path + 'test.csv')
+# print(train_set.shape) # (1460, 81)
+# print(test_set.shape)  # (1459, 80)
+
+
+# 수치형 변수와 범주형 변수 찾기
+numerical_feats = train_set.dtypes[train_set.dtypes != "object"].index
+categorical_feats = train_set.dtypes[train_set.dtypes == "object"].index
+# print("Number of Numberical features: ", len(numerical_feats)) # 38
+# print("Number of Categorical features: ", len(categorical_feats)) # 43
+
+# 변수명 출력
+print(train_set[numerical_feats].columns)      
+print("*"*80)
+print(train_set[categorical_feats].columns)
+
+# Index(['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond',
+#        'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2',
+#        'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+#        'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
+#        'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
+#        'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+#        'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal',
+#        'MoSold', 'YrSold', 'SalePrice'],
+#       dtype='object')
+# *******************************************************************************
+# Index(['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities',
+#        'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2',
+#        'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st',
+#        'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation',
+#        'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
+#        'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual',
+#        'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual',
+#        'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature',
+#        'SaleType', 'SaleCondition'],
+#       dtype='object')
+
+
+# 이상치 탐색 및 제거
+def detect_outliers(df, n, features):
+    outlier_indices = []
+    for col in features:
+        Q1 = np.percentile(df[col], 25)
+        Q3 = np.percentile(df[col], 75)
+        IQR = Q3 - Q1
+        
+        outlier_step = 1.5 * IQR
+        
+        outlier_list_col = df[(df[col] < Q1 - outlier_step) | (df[col] > Q3 + outlier_step)].index
+        outlier_indices.extend(outlier_list_col)
+    outlier_indices = Counter(outlier_indices)
+    multiple_outliers = list(k for k, v in outlier_indices.items() if v > n)
+        
+    return multiple_outliers
+        
+Outliers_to_drop = detect_outliers(train_set, 2,['Id', 'MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual',
+       'OverallCond', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1',
+       'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF',
+       'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath',
+       'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
+       'Fireplaces', 'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF',
+       'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea',
+       'MiscVal', 'MoSold', 'YrSold'])
+
+# categorical_feats 살펴보기
+# for catg in list(categorical_feats) :
+#     print(train_set[catg].value_counts())
+#     print('#'*50)
+
+# saleprice와 관련이 큰 변수들 끼리 정리.
+num_strong_corr = ['SalePrice','OverallQual','TotalBsmtSF','GrLivArea','GarageCars',
+                   'FullBath','YearBuilt','YearRemodAdd']
+
+num_weak_corr = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallCond', 'MasVnrArea', 'BsmtFinSF1',
+                 'BsmtFinSF2', 'BsmtUnfSF', '1stFlrSF', '2ndFlrSF','LowQualFinSF', 'BsmtFullBath',
+                 'BsmtHalfBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
+                 'Fireplaces', 'GarageYrBlt', 'GarageArea', 'WoodDeckSF','OpenPorchSF',
+                 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold']
+
+catg_strong_corr = ['MSZoning', 'Neighborhood', 'Condition2', 'MasVnrType', 'ExterQual',
+                    'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType']
+
+catg_weak_corr = ['Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 
+                  'LandSlope', 'Condition1',  'BldgType', 'HouseStyle', 'RoofStyle', 
+                  'RoofMatl', 'Exterior1st', 'Exterior2nd', 'ExterCond', 'Foundation', 
+                  'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 
+                  'HeatingQC', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 
+                  'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 
+                  'SaleCondition' ]
+
+
+# 데이터 결측 처리
+cols_fillna = ['PoolQC','MiscFeature','Alley','Fence','MasVnrType','FireplaceQu',
+               'GarageQual','GarageCond','GarageFinish','GarageType', 'Electrical',
+               'KitchenQual', 'SaleType', 'Functional', 'Exterior2nd', 'Exterior1st',
+               'BsmtExposure','BsmtCond','BsmtQual','BsmtFinType1','BsmtFinType2',
+               'MSZoning', 'Utilities']
+# NaN을 없다는 의미의 None 데이터로 바꾼다.
+for col in cols_fillna:
+    train_set[col].fillna('None',inplace=True)
+    test_set[col].fillna('None',inplace=True)
+
+# 결측치의 처리정도 확인.
+total = train_set.isnull().sum().sort_values(ascending=False)
+percent = (train_set.isnull().sum()/train_set.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(5)
+
+# 결측치 확인
+# print(train_set.isnull().sum().sum(), test_set.isnull().sum().sum())
+
+# 수치형 변수들 평균값으로 대체.
+train_set.fillna(train_set.mean(), inplace=True)
+test_set.fillna(test_set.mean(), inplace=True)
+
+total = train_set.isnull().sum().sort_values(ascending=False)
+percent = (train_set.isnull().sum()/train_set.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(5)
+
+# 결측치 확인
+# print(train_set.isnull().sum().sum(), test_set.isnull().sum().sum())
+
+# SalePrice'와의 상관관계가 약한 모든 변수를 삭제
+id_test = test_set['Id']
+
+to_drop_num  = num_weak_corr
+to_drop_catg = catg_weak_corr
+
+cols_to_drop = ['Id'] + to_drop_num + to_drop_catg 
+
+for df in [train_set, test_set]:
+    df.drop(cols_to_drop, inplace= True, axis = 1)
+    
+# print(train_set.head())
+
+# 수치형 변환을 위해 각 변수들의 범주들을 그룹화 시킴.
+
+# 'MSZoning'
+msz_catg2 = ['RM', 'RH']
+msz_catg3 = ['RL', 'FV'] 
+
+
+# Neighborhood
+nbhd_catg2 = ['Blmngtn', 'ClearCr', 'CollgCr', 'Crawfor', 'Gilbert', 'NWAmes', 'Somerst', 'Timber', 'Veenker']
+nbhd_catg3 = ['NoRidge', 'NridgHt', 'StoneBr']
+
+# Condition2
+cond2_catg2 = ['Norm', 'RRAe']
+cond2_catg3 = ['PosA', 'PosN'] 
+
+# SaleType
+SlTy_catg1 = ['Oth']
+SlTy_catg3 = ['CWD']
+SlTy_catg4 = ['New', 'Con']
+
+# 범주별 수치형 변환 실행
+for df in [train_set, test_set]:
+    
+    df['MSZ_num'] = 1  
+    df.loc[(df['MSZoning'].isin(msz_catg2) ), 'MSZ_num'] = 2    
+    df.loc[(df['MSZoning'].isin(msz_catg3) ), 'MSZ_num'] = 3        
+    
+    df['NbHd_num'] = 1       
+    df.loc[(df['Neighborhood'].isin(nbhd_catg2) ), 'NbHd_num'] = 2    
+    df.loc[(df['Neighborhood'].isin(nbhd_catg3) ), 'NbHd_num'] = 3    
+
+    df['Cond2_num'] = 1       
+    df.loc[(df['Condition2'].isin(cond2_catg2) ), 'Cond2_num'] = 2    
+    df.loc[(df['Condition2'].isin(cond2_catg3) ), 'Cond2_num'] = 3    
+    
+    df['Mas_num'] = 1       
+    df.loc[(df['MasVnrType'] == 'Stone' ), 'Mas_num'] = 2 
+    
+    df['ExtQ_num'] = 1       
+    df.loc[(df['ExterQual'] == 'TA' ), 'ExtQ_num'] = 2     
+    df.loc[(df['ExterQual'] == 'Gd' ), 'ExtQ_num'] = 3     
+    df.loc[(df['ExterQual'] == 'Ex' ), 'ExtQ_num'] = 4     
+   
+    df['BsQ_num'] = 1          
+    df.loc[(df['BsmtQual'] == 'Gd' ), 'BsQ_num'] = 2     
+    df.loc[(df['BsmtQual'] == 'Ex' ), 'BsQ_num'] = 3     
+ 
+    df['CA_num'] = 0          
+    df.loc[(df['CentralAir'] == 'Y' ), 'CA_num'] = 1    
+
+    df['Elc_num'] = 1       
+    df.loc[(df['Electrical'] == 'SBrkr' ), 'Elc_num'] = 2 
+
+
+    df['KiQ_num'] = 1       
+    df.loc[(df['KitchenQual'] == 'TA' ), 'KiQ_num'] = 2     
+    df.loc[(df['KitchenQual'] == 'Gd' ), 'KiQ_num'] = 3     
+    df.loc[(df['KitchenQual'] == 'Ex' ), 'KiQ_num'] = 4      
+    
+    df['SlTy_num'] = 2       
+    df.loc[(df['SaleType'].isin(SlTy_catg1) ), 'SlTy_num'] = 1  
+    df.loc[(df['SaleType'].isin(SlTy_catg3) ), 'SlTy_num'] = 3  
+    df.loc[(df['SaleType'].isin(SlTy_catg4) ), 'SlTy_num'] = 4
+    
+# 기존 범주형 변수와 새로 만들어진 수치형 변수 역시 유의하지 않은 것들 삭제.
+train_set.drop(['MSZoning','Neighborhood' , 'Condition2', 'MasVnrType', 'ExterQual', 'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType', 'Cond2_num', 'Mas_num', 'CA_num', 'Elc_num', 'SlTy_num'], axis = 1, inplace = True)
+test_set.drop(['MSZoning', 'Neighborhood' , 'Condition2', 'MasVnrType', 'ExterQual', 'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType', 'Cond2_num', 'Mas_num', 'CA_num', 'Elc_num', 'SlTy_num'], axis = 1, inplace = True)
+  
+x = train_set.drop(['SalePrice'], axis=1)
+y = train_set['SalePrice']
+USE_CUDA = torch.cuda.is_available()
+DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
+
+x = torch.FloatTensor(x.values)
+y = torch.FloatTensor(y.values).unsqueeze(1)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,
+                                                    train_size=0.8,shuffle=True,
+                                                    random_state=66)
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+x_train = torch.FloatTensor(x_train).to(DEVICE)
+x_test = torch.FloatTensor(x_test).to(DEVICE)
+y_train = torch.FloatTensor(y_train).to(DEVICE)
+y_test = torch.FloatTensor(y_test).to(DEVICE)
+
+# print(x_train.size())
+# exit()
+
+#2. 모델
+# model = nn.Sequential(
+#     nn.Linear(12,100),
+#     nn.ReLU(),
+#     nn.Linear(100,200),
+#     nn.ReLU(),
+#     nn.Linear(200,150),
+#     nn.ReLU(),
+#     nn.Linear(150,50),
+#     nn.ReLU(),
+#     nn.Linear(50,1)).to(DEVICE)
+
+class Model(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        # super().__init__()
+        super(Model, self).__init__()
+        self.linear1 = nn.Linear(input_dim,100)
+        self.linear2 = nn.Linear(100, 200)        
+        self.linear3 = nn.Linear(200, 150)
+        self.linear4 = nn.Linear(150, 50)
+        self.linear5 = nn.Linear(50, output_dim)        
+        self.relu = nn.ReLU()
+        
+    def forward(self, input_size):
+        x = self.linear1(input_size)
+        x = self.relu(x)
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
+        x = self.relu(x)
+        x = self.linear4(x)
+        x = self.relu(x)
+        x = self.linear5(x)
+        return x 
+
+model = Model(12,1).to(DEVICE)
+
+#3. 컴파일,훈련
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(),lr=0.001)
+
+def train(model,criterion,optimizer,x_train,y_train):
+    model.train()
+    optimizer.zero_grad()
+    prediction = model(x_train)
+    loss = criterion(prediction,y_train)
+    loss.backward()
+    optimizer.step()
+    return loss.item()
+
+def r2_score(y_test,y_pred):
+    u = ((y_test-y_pred)**2).sum()
+    v = ((y_test-y_test.mean())**2).sum()
+    return 1-u/v
+
+epochs = 1000
+for epoch in range(1, epochs+1):
+    loss = train(model,criterion,optimizer,x_train,y_train)
+    if epoch % 100 == 0:
+        print('Epoch {:4d}/{} Loss: {:.6f} r2_score: {:.4f}'.format(
+            epoch,epochs,loss,r2_score(y_test,model(x_test))))
+        
+#4. 평가,예측
+def evaluate(model,criterion,x_test,y_test):
+    model.eval()
+    with torch.no_grad():
+        prediction = model(x_test)
+        loss = criterion(prediction,y_test)
+    return loss.item()
+
+loss = evaluate(model,criterion,x_test,y_test)
+
+print('Loss: {:.6f}'.format(loss))
+print('r2_score: {:.4f}'.format(r2_score(y_test,model(x_test))))
+
+# Loss: 1544913792.000000
+# r2_score: 0.7298
