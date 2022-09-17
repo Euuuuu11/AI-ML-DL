101567b7541bedaedff43e2f5ce0a527d1969781
Jaegeun <zkfmakekd2@naver.com>
2022-09-17 17:57:13 +0900

    commit

--- torch/torch12_DataLoader_09_california.py
+++ torch/torch12_DataLoader_09_california.py
@@ -75,10 +75,10 @@ class Model(nn.Module):
         # super().__init__()
         super(Model, self).__init__()
         self.linear1 = nn.Linear(input_dim,100)
-        self.linear2 = nn.Linear(100, 200)        
-        self.linear3 = nn.Linear(200, 150)
-        self.linear4 = nn.Linear(150, 50)
-        self.linear5 = nn.Linear(50, output_dim)        
+        self.linear2 = nn.Linear(100, 64)        
+        self.linear3 = nn.Linear(64, 32)
+        self.linear4 = nn.Linear(32, 16)
+        self.linear5 = nn.Linear(16, output_dim)        
         self.relu = nn.ReLU()
         
     def forward(self, input_size):
