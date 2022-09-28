3309c04f188b08bfdf02e276b036750470a63aa6
Jaegeun <zkfmakekd2@naver.com>
2022-09-28 20:08:36 +0900

    commit

--- torch/torch12_DataLoader_04_wine.py
+++ torch/torch12_DataLoader_04_wine.py
@@ -47,13 +47,13 @@ train_set = TensorDataset(x_train, y_train) # x와 y를 합친다
 test_set = TensorDataset(x_test, y_test) # x와 y를 합친다
 
 print(train_set)    # <torch.utils.data.dataset.TensorDataset object at 0x0000019493120CA0>
-print('='*80)
+print('='*60)
 print(train_set[0])
-print('='*80)
+print('='*60)
 print(train_set[0][0])
-print('='*80)
+print('='*60)
 print(len(train_set))   # 398
-print('='*80)
+print('='*60)
 
 train_loader = DataLoader(train_set, batch_size=40, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=40, shuffle=False)
