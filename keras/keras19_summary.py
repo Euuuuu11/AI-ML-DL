b51e4569279ae7220dfab12bb5f1ab3a4dd2907b
Jaegeun <zkfmakekd2@naver.com>
2022-10-03 20:09:43 +0900

    commit

--- keras/keras19_summary.py
+++ keras/keras19_summary.py
@@ -10,10 +10,10 @@ y = np.array([1,2,3])
 #2. 모델구성
 
 model = Sequential()
-model.add(Dense(5, input_dim=1))
-model.add(Dense(3,activation='relu'))   
+model.add(Dense(16, input_dim=1))
+model.add(Dense(16,activation='relu'))   
+model.add(Dense(8,activation='relu'))    
 model.add(Dense(4,activation='relu'))    
-model.add(Dense(2,activation='relu'))    
 model.add(Dense(1))    
  
 model.summary() #연산량을 보여준다
