bfcb4b9ca3a1eb2d113b1bc3b53fb06c0f359599
Jaegeun <zkfmakekd2@naver.com>
2022-10-04 18:00:13 +0900

    commit

--- keras/keras11_validation2.py
+++ keras/keras11_validation2.py
@@ -11,7 +11,7 @@ y_train = y[:7]
 x_test = x[7:13]
 y_test = y[7:13]
 x_val = x[13:]
-y_bal = y[13:]
+y_val = y[13:]
 # print(x_train,y_train,x_test,y_test,x_val,y_bal) 
 # [1 2 3 4 5 6 7] [1 2 3 4 5 6 7] [ 8  9 10 11 12 13] [ 8  9 10 11 12 13] [14 15 16] [14 15 16]
 # x_train = np.array(range(1, 11))  # 훈련
