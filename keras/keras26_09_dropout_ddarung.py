cfaaf2a6cda618cca03ad495cdfcee55dcac5f1e
Jaegeun <zkfmakekd2@naver.com>
2022-10-16 20:48:58 +0900

    commit

--- keras/keras26_09_dropout_ddarung.py
+++ keras/keras26_09_dropout_ddarung.py
@@ -46,12 +46,12 @@ x_test = scaler.transform(x_test)
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(100, input_dim=9,activation='relu'))
-model.add(Dense(100))
+model.add(Dense(128, input_dim=9,activation='relu'))
+model.add(Dense(64))
 model.add(Dropout(0.3))
-model.add(Dense(100,activation='relu'))
+model.add(Dense(64,activation='relu'))
 model.add(Dropout(0.3))
-model.add(Dense(100))
+model.add(Dense(32))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
