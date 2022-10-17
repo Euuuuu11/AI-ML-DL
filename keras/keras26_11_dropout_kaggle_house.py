95966b22cf0c644cbdb2bb6053a706098c3b6a11
Jaegeun <zkfmakekd2@naver.com>
2022-10-17 11:12:33 +0900

    commit

--- keras/keras26_11_dropout_kaggle_house.py
+++ keras/keras26_11_dropout_kaggle_house.py
@@ -235,15 +235,15 @@ x_test = scaler.transform(x_test)
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(100, input_dim=12,activation='relu'))
+model.add(Dense(128, input_dim=12,activation='relu'))
 model.add(Dropout(0.3))
-model.add(Dense(80))
+model.add(Dense(64))
 model.add(Dropout(0.3))
-model.add(Dense(80,activation='relu'))
+model.add(Dense(64,activation='relu'))
 model.add(Dropout(0.3))
-model.add(Dense(50))
-model.add(Dense(50,activation='relu'))
-model.add(Dense(10))
+model.add(Dense(32))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(16))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
